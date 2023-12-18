757e2aea3ae0e9bdf133b96d35f03ed27d28e3f8
aditya <meenakshi.bhtt@gmail.com>
2023-12-18 20:15:40 +0530

    MVP ready

--- main.py
+++ main.py
@@ -1,28 +1,69 @@
 from Src.data_loader import DataLoader 
-from Src.data_transformer import DataTransformation
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import mlflow
+import mlflow.pytorch
 
 
 #Get the data 
 data=DataLoader('Data\AP001.csv')
-data=data.load_drop_na()
-print(data.shape)
-data_transform=DataTransformation(data)
-data_transform_x,data_transform_y=data_transform.transform()
-X_train,X_test,y_train,y_test,X_val,y_val=data_transform.split_data(data_transform_x,data_transform_y)
-print (X_train)
-print (y_train)
-print (X_test)
-print (y_test)
-print (X_val)
-print (y_val)
+data_cleaned=data.load_drop_na()
 
-X_train,X_test,y_train,y_test,X_val,y_val=data_transform.split_data_pytorch_tensors(X_train,X_test,y_train,y_test,X_val,y_val)
-print (X_train.size())
-print (y_train.size())
-print (X_test.size())
-print (y_test.size())
-print (X_val.size())
-print (y_val.size())
+#Train Test and Split 
+X=data_cleaned.drop(['PM2.5 (ug/m3)'],axis=1)
+y=data_cleaned['PM2.5 (ug/m3)']
+# Split into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-result=data_transform.create_tensor_dataset(X_train,X_test,y_train,y_test,X_val,y_val)
-print(result)
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+#Define a Neural Network
+class RegressionNetwork(nn.Module):
+    def __init__(self,input_size):
+        super(RegressionNetwork, self).__init__()
+        self.fc1=nn.Linear(input_size,128)
+        self.relu=nn.ReLU()
+        self.fc2=nn.Linear(128,1)
+
+    def forward(self,x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+    
+
+input_size=X_train.shape[1]
+
+model=RegressionNetwork(input_size)
+criterion=nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)  # Adam optimizer with learning rate 0.001
+
+# Training the model
+num_epochs = 500
+
+for epoch in range(num_epochs):
+    inputs = torch.tensor(X_train, dtype=torch.float32)
+    labels = torch.tensor(y_train.values, dtype=torch.float32)
+
+    optimizer.zero_grad()  # Clears the gradients of all optimized tensors
+    outputs = model(inputs)  # Forward pass
+    loss = criterion(outputs, labels.view(-1, 1))  # Calculate the loss
+    loss.backward()  # Backward pass (compute gradients)
+    optimizer.step()  # Update weights
+
+    if (epoch + 1) % 10 == 0:
+        
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+# Evaluate the model
+with torch.no_grad():
+    model.eval()
+    test_inputs = torch.tensor(X_test, dtype=torch.float32)
+    predictions = model(test_inputs).squeeze().numpy()
+
